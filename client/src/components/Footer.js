348d10b4774ec85d4ed1b7f93e62fd01e461d2e7
Bethwel123 <bethwellangat654@gmail.com>
2025-03-16 14:14:00 +0300

    Added link

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
       <Container className="text-center">
         <div className="mb-3">
           <a href="https://github.com/Bethwel123" className="text-light mx-3 fs-4" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
-          <a href="https://linkedin.com/in/bethwelLangat044b" className="text-light mx-3 fs-4" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
+          <a href="https://linkedin.com/in/bethwellangat044b" className="text-light mx-3 fs-4" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
           <a href="https://twitter.com/BethwelLan28728" className="text-light mx-3 fs-4" target="_blank" rel="noopener noreferrer"><FaTwitter /></a>
           <a href="https://instagram.com/its_me_bethwel" className="text-light mx-3 fs-4" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
         </div>
